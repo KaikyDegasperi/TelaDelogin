2266422daaf2d4f082ab0d8b2be05f2c8d881c54
KaikyDegasperi <victorkaiky.degasperi@gmail.com>
2023-02-11 19:31:42 -0300

    Projeto Tela de Login
    Commit com estilo, adicionar media querie e terminar script

---
+++ script.js
@@ -0,0 +1,40 @@
+let usuario= document.getElementById("campo")
+let password= document.getElementById("senha")
+let btn=document.querySelector("a")
+//funcao para verificar se no envio o campo está limpo
+
+btn.addEventListener('click',()=>{
+    if(usuario.value.length==0){
+        alert('coloque seu email')
+    }else{
+        return true
+    }
+})
+//verifica os elementos do dominio e usuario
+function validar(field){
+    
+   
+    let user= field.value.substring(0,field.value.indexOf('@'))
+    let domain= field.value.substring(field.value.indexOf('@'),field.value.length)
+    if((user.search('@')==-1)&&(domain.search('@')==0)){
+        
+        document.querySelector('.txtEmail').innerHTML=''
+        let p=document.createElement('p');
+        let txt=document.createTextNode('Email valido');
+        p.appendChild(txt)
+        document.querySelector('.txtEmail').appendChild(p);
+    } else {
+        
+        document.querySelector('.txtEmail').innerHTML=''
+       let p=document.createElement('p');
+       let txt=document.createTextNode('invalido')
+       p.appendChild(txt)
+       document.querySelector('.txtEmail').appendChild(p);
+    }
+}
+usuario.addEventListener('blur',()=>{
+    validar(usuario);
+})
+password.addEventListener('blur',()=>{
+    console.log('senha')
+})
